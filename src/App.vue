82b53f850c2901da761d4031b6f3a700cf54a174
Woobzor <vladychin.v.i.2.21@gmail.com>
2023-03-16 20:51:21 +0100

    Подключение бокового меню (drawer)

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,20 @@
+<script>
+export default {
+  data() {
+    return {
+      drawer: false,
+    };
+  },
+};
+</script>
+
 <template>
   <v-app>
-    <v-navigation-drawer app></v-navigation-drawer>
+    <v-navigation-drawer app v-model="drawer"> </v-navigation-drawer>
+
     <v-app-bar app dark color="primary">
-      <v-app-bar-nav-icon></v-app-bar-nav-icon>
-      <v-spacer></v-spacer>
+      <v-app-bar-nav-icon @click="drawer = !drawer"> </v-app-bar-nav-icon>
+      <v-spacer> </v-spacer>
       <v-toolbar-items class="hidden-sm-and-down">
         <v-btn text
           ><v-icon start icon="mdi-cake-variant"></v-icon> Link One</v-btn
@@ -12,12 +23,9 @@
         <v-btn text>Link Three</v-btn>
       </v-toolbar-items>
     </v-app-bar>
+
     <v-main> </v-main>
   </v-app>
 </template>
-
-<script>
-export default {};
-</script>
 
 <style></style>
