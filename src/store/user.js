d17b49addccc7a4c8d0ece1e6e5ecd292b278266
Woobzor <vladychin.v.i.2.21@gmail.com>
2023-06-16 22:12:57 +0100

    Создание getter isUserLoggedIn

--- src/store/user.js
+++ src/store/user.js
@@ -64,6 +64,9 @@ export default {
 	getters: {
         user(state) {
             return state.user;
-	}
+        },
+        isUserLoggedIn (state) {
+            return state.user !== null
+        }
     }
 }
